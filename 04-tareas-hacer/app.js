31591e51150509839e51d27db6e126242ce92c19
DanielHenryL <lagunasdaniel001@gmail.com>
2023-08-01 16:14:22 -0500

    creando logica de las tareas, models de las tareas con clases, etc

--- 04-tareas-hacer/app.js
+++ 04-tareas-hacer/app.js
@@ -1,16 +1,18 @@
 require('colors')
 
 const { inquirerMenu, pausa } = require('./helpers/inquirer');
+const Tareas = require('./models/tareas');
 
 
-
-console.clear(  )
 
 const main = async() =>{
     let opt = '';
     do{
         opt = await inquirerMenu();
         console.log( { opt } );
+        
+
+
         await pausa();
     }while( opt !== '0' );
 }
