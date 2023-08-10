eddf0243ca5c936a3312055881d732f6c9475a5d
DanielHenryL <lagunasdaniel001@gmail.com>
2023-08-10 18:55:07 -0500

    Intorduccion a expres y servir contenido estatico

--- 06-webserver/app.js
+++ 06-webserver/app.js
@@ -1,16 +1,19 @@
 const express = require('express')
 const app = express()
 const port = 8080;
-app.get('/', function (req, res) {
-  res.send('Home Page')
-})
+
+// Servir contenido estatico  , NOTA: las rutas staticas tiene prioridad con otras rutas; esto quiere
+// decir que buscara todas las rutas em la carpeta public luego las otras rutas que se hayan definido.
+app.use( express.static('public'))
+
+
 
 app.get('/hola-mundo', ( req, res ) => {
     res.send('Hola mundo')
 })
 
 app.get('*', ( req, res ) => {
-    res.send('404 | Page not found')
+    res.sendFile(__dirname + '/public/404.html')  // le expecificas un archivo
 })
 
 app.listen(port, () => {
