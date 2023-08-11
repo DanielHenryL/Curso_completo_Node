3116feab915f022e798850e6383445b187ecb3c6
DanielHenryL <lagunasdaniel001@gmail.com>
2023-08-10 22:10:36 -0500

    handlebars

--- 06-tarea/app.js
+++ 06-tarea/app.js
@@ -3,7 +3,16 @@ const app = express();
 const port = 8080
 
 
+app.set('view engine', 'hbs');
+
+
 app.use( express.static('public'));
+
+app.get('/', ( req, res) => {
+    res.render('home');
+});
+
+
 
 app.get('/generic', ( req, res) => {
     res.sendFile( __dirname + '/public/generic.html')
@@ -11,9 +20,7 @@ app.get('/generic', ( req, res) => {
 app.get('/elements', ( req, res) => {
     res.sendFile( __dirname + '/public/elements.html')
 });
-// app.get('/generic', ( req, res) => {
-//     res.sendFile( __dirname + '/public/generic.html')
-// });
+
 
 app.listen( port, () => {
     console.log( `Escuchando desde el puerto ${ port }` )
