166573d1521c9370d6b2d499fe9b0aa60dc3b082
DanielHenryL <lagunasdaniel001@gmail.com>
2023-09-02 22:41:16 -0500

    Actualizar imagen del usuario y producto, middleware para cuando no envien el archivo

--- 07-restserver/routes/uploads.js
+++ 07-restserver/routes/uploads.js
@@ -1,13 +1,20 @@
 const { Router } = require('express');
 const { check } = require('express-validator');
 
-const { validarCampos } = require('../middlewares/validar-campos');
-const { cargarArchivo } = require('../controllers/uploads');
+const { cargarArchivo, actualizarArchivo } = require('../controllers/uploads');
+const { coleccionPermitidas, existeUsuarioOrproductoById } = require('../helpers');
+const { validarArchivoSubir, validarCampos } = require('../middlewares');
 
 
 const router = Router();
 
 router.post('/', cargarArchivo );
-
+router.put('/:coleccion/:id',[
+    validarArchivoSubir,
+    check('id','No es un ID de mongo validó').isMongoId(),
+    check('coleccion').custom( ( coleccion ) => coleccionPermitidas( coleccion, ['usuarios','productos'] )),
+    // check('coleccion','No esta en la coleccion').isIn( ['usuario','producto'] ), //*otra forma de validarlo
+    validarCampos
+], actualizarArchivo)
 
 module.exports = router;
